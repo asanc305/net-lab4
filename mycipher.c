cfb028144a9893dc55096acd0eaa76c9a4e8deee
asanc305 <asanc305@fiu.edu>
2015-11-26 00:13:26 -0500

    finish encrypt

--- mycipher.c
+++ mycipher.c
@@ -5,10 +5,6 @@
 #define GetBit(var, bit) ((var & (1 << bit)) != 0)
 #define SetBit(var, bit) (var |= (1 << bit))
 
-struct {
-  unsigned char a : 8 ;
-} Ag ;
-
 void key_generator( char* key, char* k1, char* k2 )
 {
   char p [10] = {key[4], key[1], key[6], key[3], key[2], key[0], key[8], key[7], key[5], key[9]} ;
@@ -27,206 +23,266 @@ void key_generator( char* key, char* k1, char* k2 )
 
 }
 
-void xor_function( char* vector, char* plaintext, char* result, int size )
+unsigned int permutations( unsigned char x, int which )
 {
-  int i ;
-  
-  for (i=0; i < size; i++)
-  {
-    if ( (vector[i] == '0' && plaintext[i] == '1') || (vector[i] == '1' && plaintext[i] == '0') )
-      result[i] = '1' ;
-    else
-      result[i] = '0' ;
-  }
-  result[size] = '\0' ;
-
-}
-
-void initial_final_permutations( char* x, char* result, int which )
-{
-  char ip [8] = {1, 5, 2, 0, 3, 7, 4, 6} ;
-  char nip [8] = {3, 0, 2, 4, 6, 1, 7, 5} ;
+  int ip [8] = {1, 3, 0, 4, 7, 5, 2, 6} ;
+  int nip [8] = {2, 0, 6, 1, 3, 5, 7, 4} ;
+  int e_p [8] = {3, 0, 1, 2, 1, 2, 3, 0} ;
+  int p4 [4] = {7, 5, 4, 6} ;
+  unsigned char result = 0 ;
   int i ;
   
   if (which == 0)
-    for (i = 0; i < 8; i++) result[i] = x[ ip[i] ] ;
-  else
-    for (i = 0; i < 8; i++) result[i] = x[ nip[i] ] ;
+  {
+    for (i = 7; i >= 0 ; i--)
+    {
+      if (GetBit(x,ip[i]))
+        SetBit(result,i);
+    }
+  }
+  else if (which == 1)
+  {
+    for (i = 7; i >= 0 ; i--)
+    {
+      if (GetBit( x,nip[i] ))
+        SetBit( result,i );
+    }
+  }
+  else if (which == 2)
+  {
+    for (i = 7; i >= 0; i--)
+    {
+      if (GetBit( x,e_p[i] ))
+        SetBit( result,i );
+    }
+  }
+  else if (which == 3)
+  {
+    for (i = 3; i >= 0; i--)
+    {
+      if (GetBit( x,p4[i] ))
+        SetBit( result,i+4 );
+    }
+  }
     
-  result[8] = '\0' ;
+  return result ;
 }
 
-int to_int( char first, char second )
+unsigned char s_box( int r0, int c0, int r1, int c1 )
 {
-  if (first == '1' && second == '1' )
-    return 3 ;
-  else if (first == '1' && second == '0')
-    return 2 ;
-  else if (first == '0' && second == '1')
-    return 1 ;
-  else
-    return 0 ;
-}
-
-void to_char( int x, char* result )
-{
-  if (x == 3)
-    strcat( result, "11" ) ;
-  else if (x == 2)
-    strcat( result, "10" ) ;
-  else if (x == 1)
-    strcat( result, "01" ) ;
-  else
-    strcat( result, "00" ) ;
-}
-
-void f_function( char* text, char* key, char* result )
-{
-
-  char expand [8] = {text[7], text[4], text[5], text[6], text[5], text[6], text[7], text[4]} ;
   int s0 [4][4] = { {1, 0, 3, 2}, {3, 2, 1, 0}, {0, 2, 1, 3}, {3, 1, 3, 2} } ;
   int s1 [4][4] = { {0, 1, 2, 3}, {2, 0, 1, 3}, {3, 0, 1, 0}, {2, 1, 0, 3} } ;
-  char p4 [4] = {1, 3, 2, 0} ;
-  char buffer [9] ;
-  char buffer2 [4] ;
-  int i ;
-  int row ;
-  int col ;
+  unsigned char result = 0 ;
   
-  memset(buffer2, '\0', sizeof(buffer2)) ;
+  if (GetBit( s0[r0][c0],1 ))
+    SetBit( result, 7 ) ;
+  if (GetBit( s0[r0][c0],0 ))
+    SetBit( result, 6 ) ;
+    
+  if (GetBit( s1[r1][c1],1 ))
+    SetBit( result, 5 ) ;
+  if (GetBit( s1[r1][c1],0 ))
+    SetBit( result, 4 ) ;
+  
+  return result ;
+}
+
+unsigned char f_function( unsigned char text, unsigned char key )
+{
+  unsigned char buffer1 ;
+  unsigned char buffer2 ;
+  unsigned char result = 0 ;
+  int i ;
+  int row = 0;
+  int row1 = 0;
+  int col = 0;
+  int col1 = 0 ;
+  
+  //Permute P8
+  buffer1 = permutations( text, 2 ) ;
   
   //Add to key
-  xor_function( expand, key, buffer, 8 ) ;
-  
+  buffer2 = buffer1 ^ key ;
+
   //SO
-  row = to_int( buffer[0], buffer[3] ) ;
-  col = to_int( buffer[1], buffer[2] ) ;
-  to_char( s0[row][col], buffer2 ) ;
+  if (GetBit( buffer2, 7 ))
+    SetBit( row, 1 ) ;
+  if (GetBit( buffer2, 4 ))
+    SetBit( row, 0 ) ;    
   
-  //S!
-  row = to_int( buffer[4], buffer[7] ) ;
-  col = to_int( buffer[5], buffer[6] ) ;
-  to_char( s1[row][col], buffer2 ) ;
+  if (GetBit( buffer2, 6 ))
+    SetBit( col, 1 ) ;
+  if (GetBit( buffer2, 5 ))
+    SetBit( col, 0 ) ; 
+    
+  //S!  
+  if (GetBit( buffer2, 3 ))
+    SetBit( row1, 1 ) ;
+  if (GetBit( buffer2, 0 ))
+    SetBit( row1, 0 ) ;    
   
-  //Permute
-  for (i = 0; i < 4; i++)
-    buffer[i] = buffer2[ p4[i] ] ;
+  if (GetBit( buffer2, 2 ))
+    SetBit( col1, 1 ) ;
+  if (GetBit( buffer2, 1 ))
+    SetBit( col1, 0 ) ;
 
-  //Add to L
-  xor_function( text, buffer, buffer2, 4 ) ;
-  buffer2[4] = '\0' ;
+  //Input to boxes
+  buffer1 = s_box( row, col, row1, col1 ) ;
   
-  //Return L
-  strcat( result, buffer2 ); 
+  //Permute P4
+  buffer2 = permutations( buffer1, 3 ) ;
 
-  //Return R
-  for (i = 0; i < 4; i++)
-    result[i+4] = text[i+4] ;
-
+  //Add to L and append right
+  result = text ^ buffer2 ;
+  
+  return result ;
 }
 
-void switch_function( char* text, char* result )
+unsigned char switch_function( unsigned char text )
 {
   int i ;
+  unsigned char result = 0 ;
   
-  for (i = 0; i < 8; i++) 
-    result[i] = text[ (i+4) % 8 ] ;
+  for (i = 7; i >= 0; i--)
+  {
+    if (GetBit( text, i ))
+      SetBit( result, (i+4) % 8 );
+  } 
+  return result ;
 }
 
-void encrypt( char* k1, char* k2, unsigned char* plaintext, unsigned char* vector, char* result ) 
+unsigned char encrypt( unsigned char k1, unsigned char k2, unsigned char plaintext, unsigned char vector ) 
 {
-  char buffer1 [9] ;
-  char buffer2 [9] ;
-  char f_result [9] ;
- 
+  unsigned char buffer1 ;
+  unsigned char buffer2 ;
+  
   //Add to vector 
-  xor_function( plaintext, vector, buffer1, 8 ) ;
-  
+  buffer1 = plaintext ^ vector ;
+
   //Initial permutation
-  initial_final_permutations( buffer1, buffer2, 0 ) ;
-    
-  
+  buffer2 = permutations( buffer1, 0 ) ;  
+
   //fk1
-  memset( f_result, '\0', sizeof(f_result) ) ;
-  f_function( buffer2, k1, f_result) ;
+  buffer1 = f_function( buffer2, k1 ) ;
 
   //Switch
-  switch_function( f_result, buffer1 ) ;
+  buffer2 = switch_function( buffer1 ) ;
   
   //fk2
-  memset(f_result, '\0', sizeof(f_result)) ;
-  f_function( buffer1, k2, f_result ) ;
+  buffer1 = f_function( buffer2, k2 ) ;
   
   //Final Permutation
-  initial_final_permutations( f_result, result, 1 ) ;   
+  return permutations( buffer1, 1 ) ;   
+}
+
+unsigned char decrypt( unsigned char k1, unsigned char k2, unsigned char ciphertext, unsigned char vector )
+{
+  unsigned char buffer1 ;
+  unsigned char buffer2 ;
+  
+  //Add to vector 
+  buffer1 = ciphertext ^ vector ;
+  
+  //Initial permutation
+  buffer2 = permutations( buffer1, 0 ) ;
+  
+  //fk2
+  buffer1 = f_function( buffer2, k2 ) ;
+  
+  //Switch
+  buffer2 = switch_function( buffer1 ) ;
+  
+  //fk1
+  buffer1 = f_function( buffer2, k1 ) ;
+  
+  //Final Permutation
+  return permutations( buffer1, 1 ) ;
 }
 
 int main( int argc, char* argv[] )
 {
-  char k1 [9] ;
-  char k2 [9] ;
+  char key1 [8] ;
+  char key2 [8] ;
+  unsigned char k1 ;
+  unsigned char k2 ;
   unsigned char plaintext ;
-  char space[1] ;
   unsigned char buffer1 ;
   unsigned char buffer2 ;
-  int bytes_read ;
   FILE* input_file ;
   FILE* output_file ;
   
-  buffer1 = strtol( argv[1], 0, 2 ) ;
-  buffer2 = 'a' ;
-  
-  buffer1 = buffer1 ^ buffer2 ;
-  int i ;
-  for (i=7; i>=0; i--)
+  if (argc == 6 )
   {
-    if (GetBit(buffer1,i))
-      printf("1") ;
-    else
-      printf("0") ;  
-  }
-  printf("\n") ;
-  
-  /*
-  //Generate Keys
-  key_generator( argv[1], k1, k2 ) ;
-  
-  //open files
-  input_file = fopen( argv[3], "rb" ) ;
-  output_file = fopen( argv[4], "wb" ) ;  
-  
-  //check files
-  if(input_file == NULL || output_file == NULL)
-  {
-    printf("Error opening file\n") ;
-    return -1 ;
-  }
-  
-  //read first byte
-  bytes_read = fread( &plaintext, 1, 1, input_file ) ;  
-
-  buffer1 = strtol( argv[2], 0, 2 ) ;
-  
-  while( bytes_read > 0 )
-  {
-    encrypt( k1, k2, plaintext, buffer1, buffer2 ) ;
-    //fwrite( buffer2, 1, 8, output_file ) ;
+    //Generate Keys
+    key_generator( argv[2], key1, key2 ) ;
+    k1 = strtol( key1, 0, 2 ) ;
+    k2 = strtol( key2, 0, 2 ) ;
     
-    memset( plaintext, '\0', sizeof(plaintext) ) ;
-    bytes_read = fread( plaintext, 1, 1, input_file ) ;
-    strcpy( buffer1, buffer2 ) ;    
-  }
-  */
-  /*
-  int i ;
-  for (i=7; i>=0; i--)
+    //open files
+    input_file = fopen( argv[4], "rb" ) ;
+    output_file = fopen( argv[5], "wb" ) ;  
+    
+    //check files
+    if(input_file == NULL || output_file == NULL)
+    {
+      if( input_file == NULL )
+        printf("Error opening input file\n") ;
+      return -1 ;
+    }
+    
+    //read first byte
+    fread( &plaintext, 1, 1, input_file ) ;  
+
+    buffer1 = strtol( argv[3], 0, 2 ) ;
+    
+    while( !feof(input_file) )
+    {
+      buffer2 = decrypt( k1, k2, plaintext, buffer1 ) ;
+
+      fwrite( &buffer2, 1, 1, output_file ) ;
+      
+      fread( &plaintext, 1, 1, input_file ) ;
+
+      buffer1 = buffer2 ;   
+    }
+    fclose( input_file ) ;
+    fclose( output_file ) ; 
+  } 
+  else
   {
-    if (GetBit(plaintext,i))
-      printf("1") ;
-    else
-      printf("0") ;  
+    //Generate Keys
+    key_generator( argv[1], key1, key2 ) ;
+    k1 = strtol( key1, 0, 2 ) ;
+    k2 = strtol( key2, 0, 2 ) ;
+    
+    //open files
+    input_file = fopen( argv[3], "rb" ) ;
+    output_file = fopen( argv[4], "wb" ) ;  
+    
+    //check files
+    if(input_file == NULL || output_file == NULL)
+    {
+      printf("Error opening file\n") ;
+      return -1 ;
+    }
+    
+    //read first byte
+    fread( &plaintext, 1, 1, input_file ) ;  
+
+    buffer1 = strtol( argv[2], 0, 2 ) ;
+    
+    while( !feof(input_file) )
+    {
+      buffer2 = encrypt( k1, k2, plaintext, buffer1 ) ;
+
+      fwrite( &buffer2, 1, 1, output_file ) ;
+      
+      fread( &plaintext, 1, 1, input_file ) ;
+
+      buffer1 = buffer2 ;   
+    }
+    fclose( input_file ) ;
+    fclose( output_file ) ;
   }
-  printf("\n") ;*/
-  
   
 }
